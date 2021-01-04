b7a84a9a9fbd8a657e1ee9ed58dfb565f4bd99ee
ejchuang <yij1112@gmail.com>
2021-01-04 21:18:00 +0800

    modify index

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Headlines;
+use Illuminate\Support\Facades\Auth;
+
+class MainController extends Controller
+{
+    function index(){
+        $titles = Headlines::all();
+        return view("index",compact('titles'));
+    }
+
+    function insert(Request $req){
+        $title = $req ->input('title');
+        //if($title)長度>0才新增
+        $data = new Headlines;
+        $data ->title = $title;
+        $data ->save();
+        return redirect("/");
+    }
+
+    function logout(){
+        Auth::logout();
+        return redirect("/");
+    }
+}
+
--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MainController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [MainController::class,"index"]);
+Route::get('/mylogout/', [MainController::class,"logout"]);
+Route::post('/insert/', [MainController::class,"insert"]);
+Route::post('/delete/', [MainController::class,"delete"]);
+// Route::get('/', function () {
+//     return view('index');
+// });
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
